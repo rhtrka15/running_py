c1cadd4754ac12a4fd545709a75a2c2e604051c7
F Scond_ <rht1rka@gmail.com>
2019-11-07 21:06:28 +0900

    1107 파이썬 자료형 공부

---
+++ py_1107/py_1107/py_1107.py
@@ -0,0 +1,71 @@
+print(1+1)
+
+print(52, 273 , "hello")
+
+print(" 안녕하세요","저의","이름은", "김 압둘라입니다.")
+
+print(123123,'\n',123123 ,sep='                        ')
+
+print("Hello world!")
+
+a=2
+print(a)
+a="asdasdafasf"
+print(a)
+a=1.12312455
+print(a)
+a="a"
+print(a)
+
+a=input()
+
+print(a+'1234') #오버 라이딩#
+
+a=int(input())
+print(a)
+print(a+1234)
+
+print("'안녕하세요'라고 말했습니다.")
+
+print("\"배가고파요..\"라고 말했습니다.")
+
+print("asdf"*3)
+
+print("문자 선택 연산자에 대해 알아볼까요?")
+print("안녕하세요"[0])
+print("안녕하세요"[1])
+print("안녕하세요"[2])
+print("안녕하세요"[3])
+print("안녕하세요"[4])
+
+alp='abcdefghijklmnopqrstuvwxyz'
+print(alp[-26:-18])
+
+print(len(alp))
+a=15
+b=8
+
+print(a+b,a-b,a*b,a/b,a%b)
+print(a//b,a/b)
+
+print(a**b)
+
+a=input()
+print(a)
+
+tmp=''
+for i in alp:
+    tmp+=i
+print(tmp)
+
+a=input(숫자를 입력하시오:)
+print(a)
+
+a=4.1234
+b=a//1
+print("%d"(b))
+
+
+
+
+
