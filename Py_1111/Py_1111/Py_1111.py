73a971ce6df4ee04ac8a40299b27b8086a94988c
F Scond_ <rht1rka@gmail.com>
2019-11-11 22:33:44 +0900

    11,11 조건문

---
+++ Py_1111/Py_1111/Py_1111.py
@@ -0,0 +1,105 @@
+
+#AB=int(input("물건 값을 입력하시오. :"))
+
+#t=int(input("1000원 지폐 개수 :"))
+
+#fh=int(input("500원 동전의 개수 :"))
+
+#h=int(input("100원 동전의 개수 :"))
+
+#_1000=t*1000
+
+#_500=fh*500
+
+#_100=h*100
+
+#rus=_1000+_500+_100
+
+#print("500원의 개수:"rus//500)
+
+#re=rus%500
+
+#print("100원의 개수"re//100)
+
+#bb=re%100
+
+#print("50원의 개수:"bb//50)#
+
+
+#a=10
+#b=20
+#c=30
+#d=40
+
+#a,b=b,a
+
+#a,b,c,d=d,c,b,a
+
+#print(a,b,c,d)
+
+#if(a>b):
+#    print("a가 b보다 큽니다.")
+#else:
+#    print("b가 a보다 큽니다.")
+
+#import turtle
+#t=turtle,pen()
+
+#while True:
+#    direction=input("왼쪽=left, 오른쪽=right  :")
+#    if direction=="l":
+#        t.left(60)
+#        t.forward(50)
+#    if direction=="r":
+#        t.right(60)
+#        t.forward(50)
+
+#a=1
+
+#while a<10:
+#    b=int(input("정수를 입력해 주십시오 :"))
+#    c=b%2
+#    if c==1:
+#        print("홀수입니다.")
+#    else:
+#        print("짝수입니다.")
+#    a+=1
+
+#A=int(input("구입 금액을 입력하시오 :"))
+
+#if A>100000:
+#    dis=A*0.05
+#    c=A-dis
+#print("할인 가격은"c"입니다.")
+
+#to=int(input("이수한 학점수를 입력하십시오 :"))
+#pp=float(input("평점을 입력하십시오 :"))
+
+#if to>140 and pp>2.0:
+#    print("졸업이 가능합니다! ㅊㅊ")
+#else:
+#    print("졸업이 불가능 합니다..! ㅠㅠ")
+
+#import datetime
+
+#now = datetime.datetime.now()
+
+#print(now.hour,"시")
+#print(now.minute,"분")
+#print(now.second,"초")
+
+#if 3<= now.month <=5:
+#    print("이번 달은 {}월로 봄입니다!".format(now.month))
+
+#if 6<= now.month <=8:
+#    print("이번 달은 {}월로 여름입니다!".format(now.month))
+
+#if 9<= now.month <=11:
+#     print("이번 달은 {}월로 가을입니다!".format(now.month))
+
+#if 12 or 1 <= now.month <=2:
+#     print("이번 달은 {}월로 겨울입니다!".format(now.month))
+
+#from  random or import random                랜덤 사용법
+#random=randint(1,100)   random.randint(0,2)  랜덤 사용법
+
